9b17d76680788d64c3c9059fdc511de293b30ea1
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-10-11 23:03:13 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
 const App = () => {
    
+    const [query, setQuery] = useState('');
+    const [ setWeather] = useState({});
     
+    
+        if(e.key === 'Enter') {
+            const data = await fetchWeather(query);
+
+            setWeather(data);
+            setQuery('');
+        }
     }
 
     return (
